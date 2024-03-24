ff12bdb47f1042f0ace0eb6f15cf12f61188f527
Od KO <161199737+Odjargal1234@users.noreply.github.com>
2024-03-24 15:21:57 +0800

    hha

---
+++ app.js
@@ -0,0 +1,64 @@
+body {
+    background-color: black !important;
+}
+.menu{
+    width: 250px;
+    background-color: beige;
+    height: 100vh;
+    position: fixed;
+}
+.menu img{
+    height: 90px;
+    width: 90px;
+    border-radius: 50%;
+    border:3px solid black;
+    margin-top: 20px;
+   
+    margin-left: 80px;
+}
+.menu h4{
+    text-align: center;
+}
+.menu ul{
+    margin-top: 80px;
+    list-style-type: none;
+    padding: 0;
+}
+.menu ul li{
+    margin-bottom: 20px;
+    margin-top: 50px;
+}
+.menu ul li a{
+    text-decoration: none;
+    color:black;
+    font-size: 20px;
+    margin-left: 90px;
+    
+}
+#home,#about,#project,#contact{
+    width: 100%;
+    height: 100vh;
+}
+#home{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height:200px;
+    background-color: aliceblue;
+}
+#about{
+
+}
+#project  {
+
+}
+#contact{
+    
+}
+.color1{
+    margin-bottom: 30px;
+    margin-left: 30px;
+}
+.menu i{
+    margin-left: 45px;
+}
